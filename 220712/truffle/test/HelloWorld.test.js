c337cf254631ea8d4fe6d076676f42ffe415578c
bitkunst <bitkunst23@gmail.com>
2022-07-13 17:30:24 +0900

    220713 study: trigger smart contract through MetaMask

--- 220712/truffle/test/HelloWorld.test.js
+++ 220712/truffle/test/HelloWorld.test.js
@@ -1,6 +1,6 @@
 const HelloWorld = artifacts.require('HelloWorld');
 
-// contract 로 시작
+// contract 안에서 작성할 시 배포 진행
 contract('HelloWorld', (account) => {
     console.log(account); // eth.getAccounts()
 
@@ -11,12 +11,12 @@ contract('HelloWorld', (account) => {
         });
 
         it('get value', async () => {
-            console.log(await hello.value());
+            console.log(await hello.value.call());
         });
 
         it('set value', async () => {
-            await hello.setValue('bitkunst');
-            console.log(await hello.value());
+            await hello.setValue('Hello Truffle!');
+            console.log(await hello.value.call());
         });
     });
 });
